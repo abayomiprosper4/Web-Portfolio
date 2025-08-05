12fee93e66b1dd58dedbc0847a3d5fa742e3a96c
PROSPER ABAYOMI <pablocodes3846@gmail.com>
2025-08-05 11:50:40 +0100

    minor additions again....again

--- src/Header-Footer/Footer.jsx
+++ src/Header-Footer/Footer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Footer() {
   return (
     <footer className="bg-gradient-to-tr w-full from-gray-900 to-gray-950 text-center p-10 scroll-smooth text-white font-semibold">
-      <div className="ft-txt text-lg">
+      <div className="ft-txt text-md font-normal">
         2023/2024 <span className="text-orange-400 text-xl">@</span>ProCodes all rights reserved.
       </div>
     </footer>
--- src/Pages/Sec_4.jsx
+++ src/Pages/Sec_4.jsx
@@ -1,21 +1,28 @@
 import React from "react";
 import { LuLink } from "react-icons/lu";
 import { assets } from "../assets";
-import { FiFacebook, FiInstagram, FiGithub, FiMail, FiTwitter } from "react-icons/fi";
-
+import {
+  FiFacebook,
+  FiInstagram,
+  FiGithub,
+  FiMail,
+  FiLinkedin,
+} from "react-icons/fi";
+import { FaLinkedin } from "react-icons/fa";
 
 function Sec_4() {
   const Section4 = () => (
     <section
       id="contact"
-      className="min-h-[84vh] bg-hero-pattern bg-cover bg-center bg-fixed w-full flex flex-col justify-center items-center"> 
-      <a href="https://www.freepik.com/free-vector/paper-style-white-monochrome-background_15185922.htm#fromView=search&page=1&position=30&uuid=39a868c9-1f69-4b53-8845-903d05be77af"><span className="text-white">Image by freepik</span>
+      className="min-h-[84vh] bg-hero-pattern bg-cover bg-center bg-fixed w-full flex flex-col justify-center items-center"
+    >
+      <a href="https://www.freepik.com/free-vector/paper-style-white-monochrome-background_15185922.htm#fromView=search&page=1&position=30&uuid=39a868c9-1f69-4b53-8845-903d05be77af">
+        <span className="text-white">Image by freepik</span>
       </a>
       <div className="contact flex flex-col items-center text-center">
         <div className="txt03 py-8 text-4xl md:text-6xl font-semibold">
           Get In Touch <LuLink className="inline text-5xl text-orange-400" />
         </div>
-
 
         <div className="vl border-b-2 border-gray-600 w-40 h-2 my-5"></div>
         <div className="icons flex justify-center items-center space-x-6 mt-8">
@@ -24,25 +31,28 @@ function Sec_4() {
           </ContactIcon>
 
           <ContactIcon href="https://www.instagram.com/world.of_p/">
-            <FiInstagram className="text-3xl" /> 
+            <FiInstagram className="text-3xl" />
           </ContactIcon>
 
           <ContactIcon href="https://github.com/abayomiprosper4">
             <FiGithub className="text-3xl" />
           </ContactIcon>
 
+          <ContactIcon href="https://www.linkedin.com/in/prosper-abayomi-b577b8302/">
+            <FaLinkedin className="text-3xl" />
+          </ContactIcon>
+
           <ContactIcon href="https://x.com/AbayomiPro93898">
-          <img src={assets.XLogo} className="w-8 h-8" />
+            <img src={assets.XLogo} className="w-8 h-8" />
           </ContactIcon>
 
           <ContactIcon href="mailto:pablocodes3846@gmail.com">
-          <FiMail className="text-3xl" />
+            <FiMail className="text-3xl" />
           </ContactIcon>
         </div>
       </div>
     </section>
   );
-
 
   const ContactIcon = ({ href, children }) => (
     <a
@@ -57,4 +67,3 @@ function Sec_4() {
 }
 
 export default Sec_4;
-
